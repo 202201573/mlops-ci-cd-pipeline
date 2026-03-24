b999751033bfe9d8bcd968854ef1e84e1be8d36c
Your GitHub Username <your-email@example.com>
2026-03-24 22:45:47 +0200

    Final working

--- train.py
+++ train.py
@@ -1,18 +1,15 @@
 import mlflow
 import random
 
-# Use local MLflow (no server needed)
 mlflow.set_tracking_uri("file:./mlruns")
 
 with mlflow.start_run() as run:
-    # Change this value to test
-    accuracy = 0.80  # <-- change to 0.90 later
+    accuracy = 0.80  # change later to 0.90
 
     mlflow.log_metric("accuracy", accuracy)
 
     print("Accuracy:", accuracy)
     print("Run ID:", run.info.run_id)
 
-    # Save run ID
     with open("model_info.txt", "w") as f:
         f.write(run.info.run_id)
