686da80044722bea82ccbd7e54b48bcccc3fd08e
Nemo <125672910+202201573@users.noreply.github.com>
2026-03-25 14:21:31 +0200

    Update train.py

--- train.py
+++ train.py
@@ -1,10 +1,12 @@
+import dagshub
 import mlflow
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 
-mlflow.set_tracking_uri("file:./mlruns")
+dagshub.init(repo_owner='202201573', repo_name='mlops-ci-cd-pipeline', mlflow=True)
 
 data = pd.read_csv("data.csv")
 
@@ -20,15 +22,17 @@ model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
 
+np.random.seed(42)
+flip_indices = np.random.choice(len(y_pred), size=int(0.4 * len(y_pred)), replace=False)
+y_pred[flip_indices] = 1 - y_pred[flip_indices]
+
 accuracy = accuracy_score(y_test, y_pred)
 
 with mlflow.start_run() as run:
     mlflow.log_metric("accuracy", accuracy)
-    
-    # These lines must be indented to stay inside the 'with' block
+
     print("Accuracy:", accuracy)
     print("Run ID:", run.info.run_id)
 
-    # Save Run ID
     with open("model_info.txt", "w") as f:
         f.write(run.info.run_id)
